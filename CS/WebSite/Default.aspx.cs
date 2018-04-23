063a61904ced3d3f35f34096c042a0235ed671b0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Drawing;
+
+public partial class Grid_Styles_ChangeStyleOnHtmlPreparedEvents_ChangeStyleOnHtmlPreparedEvents : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void grid_CustomUnboundColumnData(object sender,
+                DevExpress.Web.ASPxGridView.ASPxGridViewColumnDataEventArgs e) {
+        if(e.Column.FieldName == "Total") {
+            decimal price = (decimal)e.GetListSourceFieldValue("UnitPrice");
+            int quantity = Convert.ToInt32(e.GetListSourceFieldValue("Quantity"));
+            e.Value = price * quantity;
+        }
+    }
+    protected void grid_HtmlDataCellPrepared(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewTableDataCellEventArgs e) {
+        if(e.DataColumn == null || e.DataColumn.FieldName != "Total") return;
+        decimal value = (decimal)e.GetValue("Total");
+        if(value > 500)
+            e.Cell.Font.Bold = true;
+
+    }
+    protected void grid_HtmlRowPrepared(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewTableRowEventArgs e) {
+        if(e.RowType != DevExpress.Web.ASPxGridView.GridViewRowType.Data) return;
+        decimal value = (decimal)e.GetValue("Total");
+        if(value > 1000)
+            e.Row.ForeColor = Color.Red;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Change row and data cell styles using events
+
+
+<p>This demo shows you how to programmatically change row and cell styles using the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_HtmlRowPreparedtopic"><u>HtmlRowPrepared</u></a><strong> </strong>and <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_HtmlDataCellPreparedtopic"><u>HtmlDataCellPrepared</u></a> events.</p>
+
+<br/>
+
+
